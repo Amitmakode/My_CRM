8b8b1d3256b3b6a9001ae043887868f022a20b64
Amitmakode <amitmakode007@gmail.com>
2026-03-03 22:16:10 +0530

    Fix create_admin command

--- leads/management/commands/create_admin.py
+++ leads/management/commands/create_admin.py
@@ -2,10 +2,14 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 
 class Command(BaseCommand):
-    def handle(self, *args, **kwargs):
-        if not User.objects.filter(username='admin').exists():
-            User.objects.create_superuser('admin', 'admin@admin.com', 'Admin@123')
-            self.stdout.write('Superuser created')
-        else:
-            self.stdout.write('Superuser already exists')
+    help = 'Create superuser'
 
+    def handle(self, *args, **kwargs):
+        try:
+            if not User.objects.filter(username='admin').exists():
+                User.objects.create_superuser('admin', 'admin@admin.com', 'Admin@123')
+                self.stdout.write(self.style.SUCCESS('Superuser created successfully'))
+            else:
+                self.stdout.write(self.style.SUCCESS('Superuser already exists'))
+        except Exception as e:
+            self.stdout.write(self.style.ERROR(f'Error: {e}'))
